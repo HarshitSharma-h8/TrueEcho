f16cc4b3377c6549ff790c44ae69e1c3a4d4c14f
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-11-30 20:47:11 +0530

    add some value dinamically using js in jsx

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2023 True Feedback. All rights reserved.
+        © {new Date().getFullYear()} TrueEcho. All rights reserved.
       </footer>
     </>
   );
